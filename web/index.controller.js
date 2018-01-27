9b5b500e1765b8cb6b8d131eca36fc7856133274
Kai Du <alanduu50@gmail.com>
2018-01-27 13:34:54 +0000

    Black Fade

--- web/index.controller.js
+++ web/index.controller.js
@@ -4,7 +4,11 @@ mainApp.controller("main_controller", function($scope) {
 
     // Current level being shown ----------------------------------------------
     $scope.current_level = 'intro';
-    $scope.current_level_set = function(new_level) {
+    $scope.current_level_set = function(new_level, transition_type) {
+        if (transition_type == 'fade') {
+            $("#black").show();
+            $("#black").fadeOut();
+        }
         $scope.current_level = new_level;
     };
 
