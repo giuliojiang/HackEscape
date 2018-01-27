aa1c0e05169b13ad184864161accc54e9a70bed2
Kai Du <alanduu50@gmail.com>
2018-01-27 13:39:22 +0000

    BEEP

--- web/index.controller.js
+++ web/index.controller.js
@@ -3,7 +3,7 @@ var mainApp = angular.module("mainApp", []);
 mainApp.controller("main_controller", function($scope) {
 
     // Current level being shown ----------------------------------------------
-    $scope.current_level = 'intro';
+    $scope.current_level = 'menu';
     $scope.current_level_set = function(new_level, transition_type) {
         if (transition_type == 'fade') {
             $("#black").show();
